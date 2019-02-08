ec5933a655a420856eb7987d1e36819df10fac87
nakzz <naqibprio10@gmail.com>
2019-02-07 20:16:32 -0600

    DS_myTests passes 4/4

--- DS_My.java
+++ DS_My.java
@@ -1,46 +1,165 @@
 
-public class DS_My implements DataStructureADT {
+public class DS_My<K extends Comparable<K>, V> implements DataStructureADT {
 
-    // TODO may wish to define an inner class 
-    // for storing key and value as a pair
-    // such a class and its members should be "private"
+  // TODO may wish to define an inner class
+  // for storing key and value as a pair
+  // such a class and its members should be "private"
 
-    // Private Fields of the class
-    // TODO create field(s) here to store data pairs
+  protected class node {
+    private node next;
+    private node prev;
+    private Comparable key;
+    private Object value;
+
+    public node(Comparable k, Object v) {
+      this.key = k;
+      this.value = v;
+      this.prev = null;
+      this.next = null;
+    }
+
+    public void setPrev(node prev) {
+      this.prev = prev;
+    }
     
-    public DS_My() {
-        // TODO Auto-generated method stub
-        
+    public node getPrev() {
+      return this.prev;
+    }
+    
+    public void setNext(node next) {
+      this.next = next;
     }
 
-    @Override
-    public void insert(Comparable k, Object v) {
-        // TODO Auto-generated method stub
-        
+    public node getNext() {
+      return this.next;
     }
 
-    @Override
-    public boolean remove(Comparable k) {
-        // TODO Auto-generated method stub
-        return false;
+    public Object getValue() {
+      return this.value;
+    }
+    
+    public Comparable getKey() {
+      return this.key;
+    }
+  }
+
+
+
+  // Private Fields of the class
+  private int size;
+  private node head;
+  private node tail;
+
+  public DS_My() {
+    this.size = 0;
+    this.head = null;
+  }
+
+
+  // Add the key,value pair to the data structure and increases size.
+  // If key is null, throws IllegalArgumentException("null key");
+  // If key is already in data structure, throws RuntimeException("duplicate key");
+  // can accept and insert null values
+
+  @Override
+  public void insert(Comparable k, Object v) {
+    if (k == null) {
+      throw new IllegalArgumentException("null key");
     }
 
-    @Override
-    public boolean contains(Comparable k) {
-        // TODO Auto-generated method stub
-        return false;
+    if (contains(k)) {
+      throw new RuntimeException("duplicate key");
     }
 
-    @Override
-    public Object get(Comparable k) {
-        // TODO Auto-generated method stub
-        return null;
+    node newNode = new node(k, v);
+
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+      newNode.setPrev(null);
+    }
+    else
+      newNode.setPrev(tail);
+      tail.setNext(newNode);
+    
+    
+    this.size++;
+
+  }
+
+  // If key is found, Removes the key from the data structure and decreases size
+  // If key is null, throws IllegalArgumentException("null key") without decreasing size
+  // If key is not found, returns false.
+  @Override
+  public boolean remove(Comparable k) {
+    if (k == null) {
+      throw new IllegalArgumentException("null key");
     }
 
-    @Override
-    public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+    if (contains(k)) {
+      //change reference
+      node deleteNode = nodeAtKey(k);
+      
+      if(deleteNode.equals(this.head)) {
+        this.head = null;
+      } else {
+        deleteNode.getPrev().setNext(deleteNode.getNext());
+        deleteNode.getNext().setPrev(deleteNode.getPrev());
+      }
+      
+     
+      this.size--;
+      
+      return true;
+    } else    
+    return false;
+  }
+
+  // Returns true if the key is in the data structure
+  // Returns false if key is null or not present
+  @Override
+  public boolean contains(Comparable k) {
+    //if nodeAtKey is not null, then true else false
+    if(nodeAtKey(k) == null) return false;
+    else return true;
+  }
+
+  // Returns the value associated with the specified key
+  // get - does not remove key or decrease size
+  // If key is null, throws IllegalArgumentException("null key") 
+  //THIS ASSUMES KEY EXISTS
+  @Override
+  public Object get(Comparable k) {
+    if (k == null) {
+      throw new IllegalArgumentException("null key");
     }
+    
+    return nodeAtKey(k).getValue();
+  }
+
+  @Override
+  public int size() {
+    return this.size;
+  }
+  
+  public node nodeAtKey(Comparable k) {
+    
+    node currNode = this.head;
+    
+    
+while(currNode != null){
+      
+
+
+      if(currNode.getKey() == k) {
+        return currNode;
+      }
+      currNode = currNode.next;
+      
+}
+    
+    return null;
+    
+  }
 
 }
--- DataStructureADTTest.java
+++ DataStructureADTTest.java
@@ -1,56 +1,119 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-abstract class DataStructureADTTest<T extends DataStructureADT<String,String>> {
-	
-	private T dataStructureInstance;
-	
-	protected abstract T createInstance();
+abstract class DataStructureADTTest<T extends DataStructureADT<String, String>> {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+  private T dataStructureInstance;
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+  protected abstract T createInstance();
 
-	@BeforeEach
-	void setUp() throws Exception {
-		dataStructureInstance = createInstance();
-	}
+  @BeforeAll
+  static void setUpBeforeClass() throws Exception {}
 
-	@AfterEach
-	void tearDown() throws Exception {
-		dataStructureInstance = null;
-	}
+  @AfterAll
+  static void tearDownAfterClass() throws Exception {}
 
-	
-	@Test
-	void test00_empty_ds_size() {
-		if (dataStructureInstance.size() != 0)
-		fail("data structure should be empty, with size=0, but size="+dataStructureInstance.size());
-	}
-	
-	// TODO: implement tests 01 - 04
+  @BeforeEach
+  void setUp() throws Exception {
+    dataStructureInstance = createInstance();
+  }
 
-	// test01_after_insert_one_size_is_one
-	
-	// test02_after_insert_one_remove_one_size_is_0
-	
-	// test03_duplicate_exception_is_thrown
-	
-	// test04_remove_returns_false_when_key_not_present
+  @AfterEach
+  void tearDown() throws Exception {
+    dataStructureInstance = null;
+  }
 
-	
-	// TODO: add tests to ensure that you can detect implementation that fail
-	
-	// Tip: consider different numbers of inserts and removes and how different combinations of insert and removes
 
+  @Test
+  void test00_empty_ds_size() {
+    // System.out.println(dataStructureInstance.size());
+    if (dataStructureInstance.size() != 0)
+      fail(
+        "data structure should be empty, with size=0, but size=" + dataStructureInstance.size());
+  }
+
+
+  // test01_after_insert_one_size_is_one
+  // insert one key value pair into the data structure and then confirm that size() is 1.
+  
+  @Test
+  void test01_after_insert_one_size_is_one() {
+    dataStructureInstance.insert("second", "ABC");
+
+    if (dataStructureInstance.size() != 1)
+      fail("data structure should have 1 element, with size=1, but size="
+        + dataStructureInstance.size());
+  }
+
+  // insert one key,value pair and remove it, then confirm size is 0.
+  // test02_after_insert_one_remove_one_size_is_0
+  
+  @Test
+  void test02_after_insert_one_remove_one_size_is_0() {
+    dataStructureInstance.insert("second", "ABC");
+
+    if (dataStructureInstance.size() != 1)
+      fail("data structure should have 1 element, with size=1, but size="
+        + dataStructureInstance.size());
+
+    dataStructureInstance.remove("second");
+
+    if (dataStructureInstance.size() != 0)
+      fail(
+        "data structure should be empty, with size=0, but size=" + dataStructureInstance.size());
+  }
+
+  // insert a few key,value pairs such that one of them has the same key as an earlier one. Confirm
+  // that a RuntimeException is thrown.
+  // test03_duplicate_exception_is_thrown
+  
+  @Test
+  void test03_duplicate_exception_is_thrown() {
+
+    boolean pass = false;
+
+    try {
+      dataStructureInstance.insert("second", "ABC");
+      dataStructureInstance.insert("second", "ABC");
+    } catch (RuntimeException e) {
+      pass = true;
+    } catch (Exception e) {
+      pass = false;
+      fail("Should throw RuntimeException");
+    }
+
+
+    if (!pass)
+      fail("Should throw RuntimeException");
+  }
+
+  // insert some key,value pairs, then try removing a key that was not inserted. Confirm that the
+  // return value is false.
+  // test04_remove_returns_false_when_key_not_present
+  
+  @Test
+  void test04_remove_returns_false_when_key_not_present() {
+
+
+    if (dataStructureInstance.remove("second"))
+      fail("No element is inside. Remove should return false, but returned true instead.");
+  }
+
+
+
+  // TODO: add tests to ensure that you can detect implementation that fail
+
+  // Tip: consider different numbers of inserts and removes and how different combinations of insert
+  // and removes
+  // test remove returns true and size decrements
+
+  // insert null key
+
+  // consider different sequences of inserts and removes. Check results of method like: size(),
+  // contains(), remove(). Can you figure out if those methods work or not?
 
 }
